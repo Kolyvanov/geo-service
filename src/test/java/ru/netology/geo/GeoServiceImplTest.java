ae5d982bd888b100896efd635128389548112f43
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-09-27 22:02:40 +0300

    GeoServiceImplTest

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,33 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class GeoServiceImplTest {
+    private GeoServiceImpl geoService = new GeoServiceImpl();
+
+    @Test
+    @DisplayName("Тест локального IP")
+    void byIPLocal() {
+
+        assertNull(geoService.byIp("127.0.0.1").getCity());
+        assertNull(geoService.byIp("127.0.0.1").getCountry());
+        assertNull(geoService.byIp("127.0.0.1").getStreet());
+        assertEquals(0, geoService.byIp("127.0.0.1").getBuiling());
+    }
+
+    @DisplayName("Тест российского IP")
+    @Test
+    void byIPRussia() {
+        assertEquals(Country.RUSSIA, geoService.byIp("172.").getCountry());
+    }
+    @DisplayName("Тест американского IP")
+    @Test
+    void shouldBeUSA() {
+        assertEquals(Country.USA, geoService.byIp("96.").getCountry());
+    }
+}
